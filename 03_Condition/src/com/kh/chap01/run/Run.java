989258a0c6713de3e5437e4c32cf38b6d8a08af2
Simyoungdo04 <sim040910@gmail.com>
2026-03-17 12:00:43 +0900

    2차

---
+++ 03_Condition/src/com/kh/chap01/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.chap01.run;
+
+import com.kh.chap01.condition.ConditionIf;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		ConditionIf ci = new ConditionIf();
+		// ci.method1();
+		ci.quiz();
+	}
+
+}
