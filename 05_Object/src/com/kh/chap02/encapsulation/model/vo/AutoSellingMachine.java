53662665ab0131ddd66879f4182f0e2081e31d94
Simyoungdo04 <sim040910@gmail.com>
2026-03-19 17:50:25 +0900

    26.03.19-7차

--- 05_Object/src/com/kh/chap02/encapsulation/model/vo/AutoSellingMachine.java
+++ 05_Object/src/com/kh/chap02/encapsulation/model/vo/AutoSellingMachine.java
@@ -3,7 +3,16 @@ package com.kh.chap02.encapsulation.model.vo;
 // 하나의 클래스는 하나의 책임만 가져야한다.
 // 필드부, 생성자부, 메소드부
 public class AutoSellingMachine {
-	
+	// 필드(Field)부
+	/*
+	 * 값을 담는 부분
+	 * 
+	 * 필드 == 멤버변수
+	 * 
+	 * 객체들이 가져야할 공통적인 속성을 기술해 놓는 것
+	 * 
+	 * 
+	 */
 	
 	
 	
