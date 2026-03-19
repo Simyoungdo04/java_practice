04f0396d8c309a1d63963ca5b9395b3bad9d5055
Simyoungdo04 <sim040910@gmail.com>
2026-03-19 16:32:13 +0900

    26.03.19-4차

--- 05_Object/src/com/kh/chap01/abstraction/run/Run.java
+++ 05_Object/src/com/kh/chap01/abstraction/run/Run.java
@@ -1,6 +1,7 @@
 package com.kh.chap01.abstraction.run;
 
-import com.kh.chap01.abstraction.model.vo.Puppy;
+// import com.kh.chap01.abstraction.model.vo.Puppy;
+import com.kh.chap01.abstraction.view.PuppyCare;
 
 public class Run {
 
@@ -8,7 +9,7 @@ public class Run {
 		// 현실세계의 강아지 => 자바세상의 객체로 구현
 		// 종 : 푸들, 이름 : 나폴레옹, 나이 : 5, 몸무게 : 8, 성별 : 암컷
 		// 강아지 객체 생성해보기
-		Puppy puppy = new Puppy(); // 객체생성, 인스턴스화
+		// Puppy puppy = new Puppy(); // 객체생성, 인스턴스화
 		// 객체를 생성할 때는 new라는 예약어를 사용한다.
 		// Memory heap영역에 데이터를 생성
 		
@@ -16,13 +17,17 @@ public class Run {
 		// 값이 없는 참조자료형은 heap영역에서 null이 들어있다.
 		// System.out.println(puppy.name); // 참조
 		
-		puppy.name = "나폴레옹";
-		System.out.println(puppy.name);
+		/*
+		 * puppy.name = "나폴레옹"; 
+		 * System.out.println(puppy.name)
+		 */;
 		
 		// 종 : 방하르, 이름 : 징기스칸, 나이 : 5, 몸무게 : 40, 성별 : 수
-		Puppy khan = new Puppy();
-		khan.name = "징기스칸";
-		System.out.println(khan.name);
+		/*
+		 * Puppy khan = new Puppy(); 
+		 * khan.name = "징기스칸"; 
+		 * System.out.println(khan.name);
+		 */
 		
 		// 정수값 => 정수자료형 int, short, byte, long
 		// 실수값 => 실수자료형
@@ -36,19 +41,21 @@ public class Run {
 		// 객체 지향 프로그래밍 => 현실세계에서의 개체를 속성과 행위를 가진 객체로 생성하여
         //					 객체간의 관계를 맺고 상호작용을 통해 프로그래밍을 하는 개발*방법론*
 		
-		puppy.bark();
-		khan.bark();
+		/*
+		 * puppy.bark(); khan.bark();
+		 * 
+		 * puppy.weight = 8; khan.weight = 40;
+		 * 
+		 * System.out.println(puppy.name + "의 몸무게 : " + puppy.weight + "kg");
+		 * System.out.println(khan.name + "의 몸무게 : " + khan.weight + "kg");
+		 * 
+		 * puppy.sit(); khan.sit(); khan.sit(); System.out.println(khan.name +
+		 * "의 몸무게 : " + khan.weight + "kg");
+		 */
 		
-		puppy.weight = 8;
-		khan.weight = 40;
 		
-		System.out.println(puppy.name + "의 몸무게 : " + puppy.weight + "kg");
-		System.out.println(khan.name + "의 몸무게 : " + khan.weight + "kg");
-		
-		puppy.sit();
-		khan.sit();
-		khan.sit();
-		System.out.println(khan.name + "의 몸무게 : " + khan.weight + "kg");
+		PuppyCare pc = new PuppyCare();
+		pc.run();
 	}
 
 }
--- 05_Object/src/com/kh/chap01/abstraction/view/PuppyCare.java
+++ 05_Object/src/com/kh/chap01/abstraction/view/PuppyCare.java
@@ -1,10 +1,59 @@
 package com.kh.chap01.abstraction.view;
 
+import java.util.Scanner;
+
+import com.kh.chap01.abstraction.model.vo.Puppy;
+
 public class PuppyCare {
 	// 사용자에게 강아지의 정보를 입력받아서
 	// 입력받은 강아지의 정보를 출력해주고
 	// 강아지와 함게 놀아줄 수 있는 프로그램
 	
+	public void run() {
+		System.out.println("🦴🐕강아지 다마고치🐕🦴");
+		Scanner sc = new Scanner(System.in); // 스캐너 타입의 변수 선언 = 스캐너 객체 생성;
+		Puppy puppy = new Puppy();
+		
+		System.out.print("강아지 이름이 뭐예요? > ");
+		puppy.name = sc.nextLine();
+		System.out.print("무슨 종이예요? > ");
+		puppy.species = sc.nextLine();
+		System.out.print("암컷이예요? 수컷이예요? > ");
+		puppy.gender = sc.nextLine();
+		System.out.print("몇 살이예요? > ");
+		puppy.age = sc.nextInt();
+		System.out.print("얘는 몇 키로예요? > ");
+		puppy.weight = sc.nextInt();
+		
+		while(true) {
+			System.out.println("🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴");
+			System.out.println(puppy.name + "의 정보");
+			System.out.println("나이 : " + puppy.age);
+			System.out.println("성별 : " + puppy.gender);
+			System.out.println("종 : " + puppy.species);
+			System.out.println("몸무게 : " + puppy.weight);
+			System.out.println("🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴");
+			System.out.println();
+			break;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 	
