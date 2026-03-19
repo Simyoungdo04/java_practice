e9257dc7a2132c4424cc74d18ab10052240e4d25
Simyoungdo04 <sim040910@gmail.com>
2026-03-19 17:35:47 +0900

    26.03.19-6차

---
+++ 05_Object/src/com/kh/chap02/encapsulation/model/vo/AutoSellingMachine.java
@@ -0,0 +1,10 @@
+package com.kh.chap02.encapsulation.model.vo;
+
+// 하나의 클래스는 하나의 책임만 가져야한다.
+// 필드부, 생성자부, 메소드부
+public class AutoSellingMachine {
+	
+	
+	
+	
+}
---
+++ 05_Object/src/com/kh/chap02/encapsulation/run/Run.java
@@ -0,0 +1,7 @@
+package com.kh.chap02.encapsulation.run;
+
+public class Run {
+	public static void main(String[] args) {
+		
+	}
+}
