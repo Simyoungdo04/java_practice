40206ac52fb3d5509ebdcadee85689fcc635e73d
Simyoungdo04 <sim040910@gmail.com>
2026-03-20 12:49:39 +0900

    26.03.19-7차

--- 05_Object/src/com/kh/chap02/encapsulation/model/vo/AutoSellingMachine.java
+++ 05_Object/src/com/kh/chap02/encapsulation/model/vo/AutoSellingMachine.java
@@ -11,8 +11,62 @@ public class AutoSellingMachine {
 	 * 
 	 * 객체들이 가져야할 공통적인 속성을 기술해 놓는 것
 	 * 
-	 * 
+	 * 접근제한자 자료형 필드식별자;
 	 */
+	// 자판기 == 제품이름, 제품가격, 수량
+	private String name;
+	private int price;
+	private int amount;
+	
+	// Dependency(의존) 관계 중요*
+	
+	private void find() {
+		System.out.println(amount);
+	}
+	private void sell() {
+		amount -= 1;
+	}
+	private void add() {
+		amount += 3;
+	}
+	
+	public void open() {
+		find();
+		add();
+		find();
+		sell();
+		find();
+	}
+	
+	// 객체의 속성 값을 기록 및 수정하는 기능의 메소드 : setter()
+	/*
+	 * VO -> setter메소드를 구현하지 않음
+	 * DTO -> setter를 구현해서
+	 * 
+	 * setter 만들기 규칙
+	 * 
+	 * 1. setter메소드는 외부에서 접근이 가능해야 하기 때문에 접근제한자 public을 이용
+	 * 2. set필드명으로 식별자를 작성하며 낙타봉표기법(camelCase)를 꼭 지키도록한다.
+	 */
+	
+	// 하나의 메소드는 하나의 기능만을 수행해야한다. **
+	
+	// price필드 값을 set할 수 있는 메소드 구현
+	public void setPrice(int price) { // void 리턴타입 반환형
+						// parameter, 매개변수
+						// 매개변수는 메소드 호출 시 초기화가 강제됨**
+		System.out.println(price);
+		// Scope안에서는 해당 영역안에 있는 지역변수가 식별자 우선권을 가진다.
+		// this : 해당 객체의 주소값을 가리키는 역할
+		this.price = price;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- 05_Object/src/com/kh/chap02/encapsulation/run/Run.java
+++ 05_Object/src/com/kh/chap02/encapsulation/run/Run.java
@@ -1,7 +1,49 @@
 package com.kh.chap02.encapsulation.run;
 
+import com.kh.chap02.encapsulation.model.vo.AutoSellingMachine;
+
 public class Run {
+	// 캡슐화 (encapsulation)
+	// 객체지향의 4대 특성: 추상, 캡슐화, 상속, 가용성
+	// 속성(데이터)과 해당 데이터를 조작하는 행동(메소드)를 하나의 단위로 묶는 것
+	// 정보은닉 (information hiding)
 	public static void main(String[] args) {
+		// 객체생성 => new 키워드를 쓴다. -> 생성할 클래스 이름을 적어준다.(생성자) 
+		// 객체생성 == 메모리(heap)에 적재
+		AutoSellingMachine asm = new AutoSellingMachine();
+		// 변수 공간에 대입될 수 있는 값은 딱 9가지(null값 제외) 기본자료형
+		// 주소값 == 객체(heap영역)
+		/*asm.name = "삼양라면";
+		System.out.println(asm.name);
+		asm.price = 1100;
+		System.out.println(asm.price);
+		asm.amount = 10;
+		System.out.println(asm.amount);
+		*/
+		
+		/*
+		 * asm.find(); 
+		 * asm.add(); 
+		 * asm.find(); 
+		 * asm.sell(); 
+		 * asm.find();
+		 */
+		
+		// asm.open();
+		
+		// 필드가 외부에서는 보이지 않아서 오류가 생긴다
+		// 접근제한자를 public에서 private로 변경했기 때문이다
+		// 직접접근이 불가능 => 간접적으로 접근할 수 있는 방법을 만들어줘야함
+		
+		// getter / setter라는 메소드 구현하기
+		// 라면 가격 1500원으로 세팅하고 싶다.
+		asm.setPrice(1500); // argument, 인자값, 인수
+		
+		
+		
+		
+		
+		
 		
 	}
 }
